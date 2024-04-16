52e2e4c14223ba1cef41816fba36b31f403997f9
GuicoTubera20003 <166929746+GuicoTubera20003@users.noreply.github.com>
2024-04-16 11:05:29 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/test/TestClasseStanza.java
@@ -0,0 +1,108 @@
+package it.uniroma3.diadia.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class TestClasseStanza {
+	
+	Stanza r1= new Stanza("r1");
+	Stanza r2= new Stanza("r2");
+	Stanza r3= new Stanza("r3");
+	Stanza r4= new Stanza("r4");
+	Stanza r5= new Stanza("r5");
+	Stanza r6= new Stanza("r6");
+	
+	
+	@Test
+	public void testimpostaStanzaAdiacenteConStanzaDiPartenzaSenzaAdiacenti() {
+		
+		r1.impostaStanzaAdiacente("nord", r2);
+		
+		assertSame("errore",r2, r1.getStanzaAdiacente("nord"));
+		
+		
+	}
+	
+	@Test
+	public void testimpostaStanzaAdiacenteInUnaAdiacenzaGiaOccupata() {
+		
+		r1.impostaStanzaAdiacente("nord", r3);
+		assertEquals( "errore",r3, r1.getStanzaAdiacente("nord"));		
+	}
+	
+	
+	@Test  
+	public void testimpostaStanzaAdiacenteConStanzaCorrenteConDirezioniPiene() {
+		
+		r1.impostaStanzaAdiacente("nord", r2);
+		r1.impostaStanzaAdiacente("sud", r3);
+		r1.impostaStanzaAdiacente("est", r4);
+		r1.impostaStanzaAdiacente("ovest", r5);
+		
+		r1.impostaStanzaAdiacente("sud ovest", r6);
+		assertNull(r1.getStanzaAdiacente("sud ovest"));
+
+	}
+	
+	
+	
+	
+	Attrezzo martello= new  Attrezzo("martello", 5);
+	Attrezzo koko= new Attrezzo("koko", 12);
+	
+	//Test su HasAttrezzo
+	
+	@Test 
+	public void testHasAttrezzoNUll() {
+		assertFalse(r1.hasAttrezzo(null));
+	}
+	
+	
+	
+	
+	
+	//Test sul metodo removeAttrezzo
+	
+
+
+	@Before
+	public void insert() {
+		
+		if(!r1.hasAttrezzo("martello")) 
+			r1.addAttrezzo(martello);
+		
+	}
+
+	
+	@Test
+	public void testRemoveAttrezzoNull() {
+		assertFalse(r1.removeAttrezzo(null));
+		
+	}
+	
+	@Test
+	public void testRemoveAttrezzo() {
+		assertTrue(r1.removeAttrezzo(martello));
+		
+		
+	}
+	
+	@Test
+	public void testRemoveAttrezzoInesistente() {
+		assertFalse(r1.removeAttrezzo(koko));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
